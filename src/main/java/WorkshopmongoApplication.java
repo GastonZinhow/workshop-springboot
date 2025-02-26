e37569685d12daf9d0074a840cd6bc5d217f11c0
Matheus Gaston <theteusviana@gmail.com>
2025-02-26 17:03:27 -0300

    feat: mongodb connected with repository and service

---
+++ src/main/java/WorkshopmongoApplication.java
@@ -0,0 +1,12 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories("repository")
+public class WorkshopmongoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopmongoApplication.class, args);
+    }
+}
+
