3f81015fe382df479f1541b850cca1b44c284c89
Matheus Gaston <theteusviana@gmail.com>
2025-03-06 17:48:35 -0300

    feat: get user id

---
+++ src/main/java/com/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
