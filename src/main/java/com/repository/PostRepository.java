a8693e7c6121c058055b39ba64d42df513cb70d2
Matheus Gaston <theteusviana@gmail.com>
2025-03-06 22:04:56 -0300

    feat: @query

--- src/main/java/com/repository/PostRepository.java
+++ src/main/java/com/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.repository;
 
 import com.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.domain.User;
@@ -11,5 +12,7 @@ import java.util.List;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
     List<Post> findByTitleContainingIgnoreCase(String text);
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
 }
